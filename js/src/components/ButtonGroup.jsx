31564738d26ac50729c7358c061325701c85b767
Thomas Leconte <thomasl.dw20@gmail.com>
2024-08-13 18:50:19 +0200

    logique des evenement au click

--- js/src/components/ButtonGroup.jsx
+++ js/src/components/ButtonGroup.jsx
@@ -3,11 +3,25 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
 export default function BasicButtonGroup() {
+
+        // Fonction appelée lorsque le bouton est cliqué
+        const handleClick = (event) => {
+
+          const buttonText = event.target.textContent;
+
+          console.log('Texte du bouton cliqué :', buttonText);
+
+          if (buttonText === 'Three') {
+            alert('VIVA L\'ALGERIE');
+          }
+          
+        }
+
     return (
       <ButtonGroup variant="contained" aria-label="Basic button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
+        <Button onClick={handleClick}>One</Button>
+        <Button onClick={handleClick}>Two</Button>
+        <Button onClick={handleClick}>Three</Button>
       </ButtonGroup>
     );
   }
