31dd84c11f6da7f1061b365645d26148578a785a
Thomas Leconte <thomasl.dw20@gmail.com>
2024-08-25 18:51:01 +0200

    TEST container (taille et custom)

--- js/src/components/Container.jsx
+++ js/src/components/Container.jsx
@@ -2,13 +2,16 @@ import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import DownloadButton from "./checkout/DownloadButton.jsx";
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+      {/* <Container maxWidth="sm" sx={{ bgcolor: "#000000", width:"100vw", height: "100vh" }}> */}
+      <Container fixed sx={{ bgcolor: "#000000", width:"100", height: "80vh", marginTop: "10px" }}>
+        {/* <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} /> */}
+      <DownloadButton />
       </Container>
     </React.Fragment>
   );
