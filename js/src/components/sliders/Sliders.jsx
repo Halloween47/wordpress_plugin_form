f8bd5028af0714f2712998d646a9c04e4d267436
Thomas Leconte <thomasl.dw20@gmail.com>
2024-08-28 20:36:49 +0200

    SLIDERS OK

--- js/src/components/sliders/Sliders.jsx
+++ js/src/components/sliders/Sliders.jsx
@@ -5,21 +5,79 @@ import Container from "@mui/material/Container";
 
 import { styled } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import "./sliders.scss";
 
 const Test = styled(Grid)(() => ({
   color: "blue",
 }));
 
+const GlobalStyles = `
+.container-carousel {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: auto;
+  gap: 3rem;
+  flex-flow: column;
+  position: relative;
+}
+.carousel {
+  max-width: 80%;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  transform-style: preserve-3d;
+  transform: perspective(1000px);
+}
+.carousel .item {
+  width: 20%;
+  height: 250px;
+  max-width: 250px;
+  /* background-color: white; */
+  margin: 2px;
+  transition: 0.5s;
+  object-fit: cover;
+  filter: brightness(1);
+}
+.carousel .item:hover {
+  filter: brightness(1);
+  transform: translateZ(170px);
+}
+.carousel .item:hover + * {
+  filter: brightness(0.6);
+  transform: translateZ(120px) rotateY(25deg);
+}
+.carousel .item:hover + * + * {
+  filter: brightness(0.4);
+  transform: translateZ(50px) rotateY(15deg);
+}
+.carousel .item:has(+ *:hover) {
+  filter: brightness(0.6);
+  transform: translateZ(120px) rotateY(-25deg);
+}
+.carousel .item:has(+ * + *:hover) {
+  filter: brightness(0.4);
+  transform: translateZ(50px) rotateY(-15deg);
+}
+
+`;
+
 export default function Sliders() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm" sx={{ backgroundColor: "red" }}>
+      <Container
+        maxWidth="md"
+        sx={{
+          backgroundColor: "red",
+          width: "100%",
+          height: "auto",
+          mt: 2,
+        }}
+      >
         <div className="container-carousel">
           <div className="carousel">
-            <div className="test-sliders">TEST SLI DERNS</div>
-            {/* <img
+            <img
               class="item"
               src="https://images.pexels.com/photos/1173777/pexels-photo-1173777.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
@@ -43,15 +101,11 @@ export default function Sliders() {
               class="item"
               src="https://images.pexels.com/photos/1658967/pexels-photo-1658967.jpeg?auto=compress&cs=tinysrgb&w=600"
               alt=""
-            /> */}
-          </div>
-          <div className="code-by">
-            <h2 className="">
-              code by :- <span data="ulhas Johari">ulhas Johari</span>
-            </h2>
+            />
           </div>
         </div>
       </Container>
+      <style>{GlobalStyles}</style>
     </React.Fragment>
   );
 }
