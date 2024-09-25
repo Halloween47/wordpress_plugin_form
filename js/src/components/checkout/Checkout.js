6fd5ba71d8a3fe0d0bf24a62cbe7b0992a61e180
Thomas Leconte <thomasl.dw20@gmail.com>
2024-09-25 16:58:21 +0200

    Modification Logo Dans la zone recap

--- js/src/components/checkout/Checkout.js
+++ js/src/components/checkout/Checkout.js
@@ -177,7 +177,8 @@ export default function Checkout() {
               <img
                   src={
                     // "https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg"
-                    "https://memenza.laisne.ovh/wp-content/uploads/2023/05/memenza.jpg"
+                    // "https://memenza.laisne.ovh/wp-content/uploads/2023/05/memenza.jpg"
+                    "http://portfoliowoocommerce.local/wp-content/uploads/2024/09/memenza-removebg-preview.png"
                   }
                   style={logoStyle}
                   alt="Sitemark's logo"
