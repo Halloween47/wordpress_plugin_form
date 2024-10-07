c81ab3b45862b4306d21ede1c93e63dfb26c2878
Thomas Leconte <thomasl.dw20@gmail.com>
2024-10-07 14:03:35 +0200

    Composant memenza_sous-categories et apparition du sliders OK

--- js/src/memenza/components/MemenzaCategories.jsx
+++ js/src/memenza/components/MemenzaCategories.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sliders from './Sliders.jsx'
+import MemenzaSousCategories from './MemenzaSousCategories.jsx';
 
 function MemenzaCategories() {
+  const [showSliders, setShowSliders] = useState(false);
+
+  function handleImageClick() {
+    setShowSliders(true);
+    console.log(showSliders);
+    
+  }
   return (
     <div className='memenza-categories'>
         <h2>Choisissez votre catégorie.</h2>
-        <Sliders/>
+        <Sliders onImageClick={handleImageClick}/>
+        {showSliders && <MemenzaSousCategories />}
     </div>
   )
 }
---
+++ js/src/memenza/components/MemenzaSousCategories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sliders from './Sliders.jsx'
+
+function MemenzaSousCategories() {
+  return (
+    <div className='memenza-sous-categories'>
+        <h2>Choisissez votre sous catégorie.</h2>
+        <Sliders />
+    </div>
+  )
+}
+
+export default MemenzaSousCategories
