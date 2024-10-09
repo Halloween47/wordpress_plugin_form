8e3e4a19d3d2dc40c8311988af8f7bc27bad1f5b
Thomas Leconte <thomasl.dw20@gmail.com>
2024-10-09 22:52:07 +0200

    Composant "sous categories" : Bouton SUIVANT OK

--- js/src/memenza/components/MemenzaSousCategories.jsx
+++ js/src/memenza/components/MemenzaSousCategories.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import SlidersSousCategories from "./SlidersSousCategories.jsx";
+import { Button as BaseButton, Button, buttonClasses } from "@mui/material";
 
 const StylesOverlay = `
 .memenza-sous-categories {
   background-color: rgba(164, 111, 251, 0.95);
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 95vh;
+  top: 0%;
+  width: 95%;
+  height: 90vh;
   padding: 10px;
   display: flex;
   flex-direction: column;
@@ -25,6 +25,12 @@ function MemenzaSousCategories() {
     <div className="memenza-sous-categories">
       <h2>Choisissez votre sous catégorie</h2>
       <SlidersSousCategories />
+      <Button
+        variant="contained"
+        sx={{ position: "absolute", bottom: "5%", right: "5%" }}
+      >
+        Visuel
+      </Button>
       <style>{StylesOverlay}</style>
     </div>
   );
