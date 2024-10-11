08d066463fa8569abc9acb0601a99f4f61b01a24
Thomas Leconte <thomasl.dw20@gmail.com>
2024-10-11 14:56:27 +0200

    Composant "Choisir Visuel" : OK

--- js/src/memenza/components/MemenzaVisuel.jsx
+++ js/src/memenza/components/MemenzaVisuel.jsx
@@ -1,11 +1,13 @@
 import { Button, ButtonGroup } from "@mui/material";
 import React from "react";
 import MemenzaSousCategoriesDetails from "./MemenzaSousCategoriesDetails.jsx";
+import MemenzaChoixVisuel from "./MemenzaChoixVisuel.jsx";
 
 function MemenzaVisuel() {
   return (
     <div>
       <MemenzaSousCategoriesDetails />
+      <MemenzaChoixVisuel />
     </div>
   );
 }
