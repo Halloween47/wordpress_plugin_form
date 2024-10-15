019ffc02f46fb21db865d3065d8368051ca35548
Thomas Leconte <thomasl.dw20@gmail.com>
2024-10-15 17:21:12 +0200

    apparition du sliders sous categories

--- js/src/memenza/components/EtapeCategorie.jsx
+++ js/src/memenza/components/EtapeCategorie.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Sliders from './Sliders.jsx'
+import MemenzaSousCategories from './MemenzaSousCategories.jsx';
 
 function EtapeCategorie() {
+    const [showSliders, setShowSliders] = useState(false);
+
+    function handleImageClick() {
+        setShowSliders(true);
+      }
+    
   return (
     <div className="memenza-categories">
       <h2>Choisissez votre catégorie.</h2>
-      <Sliders />
+      <Sliders onImageClick={handleImageClick} />
+      {showSliders && <MemenzaSousCategories />}
       </div>
   )
 }
