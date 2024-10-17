4f0b559ad31a55fa1bb3d84a993c09a86cf4851f
Thomas Leconte <thomasl.dw20@gmail.com>
2024-10-17 22:47:02 +0200

    Mise en place de la structure de base de stepper

--- js/src/memenza/MemenzaFormulaire.jsx
+++ js/src/memenza/MemenzaFormulaire.jsx
@@ -17,8 +17,8 @@ function getStepContent(step) {
   switch (step) {
     case 0:
       // return <MemenzaCategories />;
-      // return <EtapeCategorie />;
-      return <EtapeVideo />;
+      return <EtapeCategorie />;
+      // return <EtapeVideo />;
     case 1:
       return <MemenzaVisuel />;
     case 2:
