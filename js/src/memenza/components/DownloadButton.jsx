2cab747c592e97044df7a2527cac3af52eda7819
Thomas Leconte <thomasl.dw20@gmail.com>
2024-10-23 20:52:23 +0200

    BUG merge

--- js/src/memenza/components/DownloadButton.jsx
+++ js/src/memenza/components/DownloadButton.jsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { styled } from "@mui/material";
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
 });
 
@@ -25,19 +25,19 @@ function DownloadButton() {
     //   Uploader son propre visuel
     // </Button>
     <Button
-    component="label"
-    role={undefined}
-    variant="contained"
-    tabIndex={-1}
-    startIcon={<CloudUploadIcon />}
-  >
-  Uploader son propre visuel
-    <VisuallyHiddenInput
-      type="file"
-      onChange={(event) => console.log(event.target.files)}
-      multiple
-    />
-  </Button>
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      startIcon={<CloudUploadIcon />}
+    >
+      Uploader son propre visuel
+      <VisuallyHiddenInput
+        type="file"
+        onChange={(event) => console.log(event.target.files)}
+        multiple
+      />
+    </Button>
   );
 }
 export default DownloadButton;
