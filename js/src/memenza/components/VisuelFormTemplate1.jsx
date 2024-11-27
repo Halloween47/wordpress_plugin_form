1046d3c17200aaac8ff4f2775f444fa3d3b0a7f5
Thomas Leconte <thomasl.dw20@gmail.com>
2024-11-27 23:48:02 +0100

    POC ETAPE VISUEL mise en place des input du form pour template 1

--- js/src/memenza/components/VisuelFormTemplate1.jsx
+++ js/src/memenza/components/VisuelFormTemplate1.jsx
@@ -1,7 +1,86 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Divider,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const VisuelFormTemplate1 = () => {
-  return <div>VisuelFormTemplate1</div>;
+  return (
+    <>
+      <div>VisuelFormTemplate1</div>;
+      <Box className="etape-video-personnalisation-visuel" spacing={2}>
+        <Typography variant="h5" gutterBottom>
+          Personnalisez votre vidéo
+        </Typography>
+        <Box
+          className="upload-media"
+          sx={{ display: "flex", alignItem: "center", my: "10px" }}
+        >
+          <Typography
+            variant="body2"
+            sx={{
+              margin: "inherit",
+              padding: "inherit",
+              display: "flex",
+              alignItem: "center",
+            }}
+          >
+            Image de fond :
+          </Typography>
+          <Button variant="contained" color="primary">
+            Choisir un fichier
+          </Button>
+        </Box>
+        <Box
+          className="upload-media"
+          sx={{ display: "flex", alignItem: "center", my: "10px" }}
+        >
+          <Typography
+            variant="body2"
+            sx={{
+              margin: "inherit",
+              padding: "inherit",
+              display: "flex",
+              alignItem: "center",
+            }}
+          >
+            Image à superposer :
+          </Typography>
+          <Button variant="contained" color="primary">
+            Choisir un fichier
+          </Button>
+        </Box>
+        <Box
+          className="upload-media"
+          sx={{ display: "flex", alignItem: "center", my: "10px" }}
+        >
+          <Typography variant="body2">Texte 1 (max 15 caractères)</Typography>
+          <TextField
+            id={`filled-basic`}
+            label={`Text`}
+            variant="filled"
+            size="small"
+          />
+        </Box>
+        <Box
+          className="upload-media"
+          sx={{ display: "flex", alignItem: "center", my: "10px" }}
+        >
+          <Typography variant="body2">Texte 2 (max 17 caractères)</Typography>
+          <TextField
+            id={`filled-basic`}
+            label={`Text`}
+            variant="filled"
+            size="small"
+          />
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default VisuelFormTemplate1;
